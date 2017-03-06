7dbacf2683157723fbe3a97ae398792dc23c400c
Andrey Kozhurin <nice@mac-mini-andrey.local>
2017-03-06 10:05:41 +0300

    project initialization

---
+++ AnimatiOS10/AnimatiOS10/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AnimatiOS10
+//
+//  Created by Andrey Kozhurin on 06/03/2017.
+//  Copyright © 2017 Andrey Kozhurin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
